e99563524c40db455d1ddd7b7e9a9d0ea51c0b58
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2019-11-14 20:40:46 +0100

    JS - add functions populateList

--- main.js
+++ main.js
@@ -10,7 +10,19 @@ function addTask(e) {
         done: false,
     }
     tasks.push(task);
+    populateList(tasks, taskList);
     this.querySelector('[name=to-do]').value = '';
+}
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+        <li>
+            <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+            <label for="item${i}">${plate.text}</label>        
+         </li>
+        `;
+    }).join('');
 }
 
 addTasks.addEventListener('submit', addTask);

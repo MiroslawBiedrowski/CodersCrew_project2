6975cd32fea5a2b3a8e35ea0c8a57ae13c0df192
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2019-11-17 12:00:53 +0100

    Change FN addTask

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var tasks = JSON.parse(localStorage.getItem('tasks1')) || [];
 
 function addTask(task) {
-    tasks.push(task);
-    localStorage.setItem('tasks1', JSON.stringify(tasks));
+    tasks.push({
+        task,
+        done: false
+    });
+    // localStorage.setItem('tasks1', JSON.stringify(tasks));
     console.log(`Masz do zrobienia poniższą poniższe rzeczy:`);
     tasks.forEach((element, index) => {
         console.log(index, element)

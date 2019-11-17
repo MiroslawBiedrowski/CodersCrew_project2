3b2911785e16ceedf8d874de3501e8d9c3de7b4f
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2019-11-17 11:50:40 +0100

    Init commit + create structure HTML + js, and FN addTask

--- main.js
+++ main.js
@@ -1,62 +1,10 @@
-const addTasks = document.querySelector('.add-to-do');
-const taskList = document.querySelector('.to-do-list');
-var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-const changeTasks = document.querySelector('.change-to-do');
+var tasks = JSON.parse(localStorage.getItem('tasks1')) || [];
 
-function addTask(e) {
-    e.preventDefault();
-    const text = (this.querySelector('[name=to-do]')).value;
-    const task = {
-        text,
-        done: false,
-    };
+function addTask(task) {
     tasks.push(task);
-    populateList(tasks, taskList);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    this.querySelector('[name=to-do]').value = '';
+    localStorage.setItem('tasks1', JSON.stringify(tasks));
+    console.log(`Masz do zrobienia poniższą poniższe rzeczy:`);
+    tasks.forEach((element, index) => {
+        console.log(index, element)
+    });
 }
-
-function populateList(plates = [], platesList) {
-    platesList.innerHTML = plates.map((plate, i) => {
-        return `
-        <li>
-            <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
-            <label for="item${i}">${plate.text} - pozycja nr ${i}</label>
-            <button class="btn-rem">Usuń</button>        
-         </li>
-        `;
-    }).join('');
-}
-
-function toggleDone(e) {
-    if (!e.target.matches('input')) return;
-    const el = e.target;
-    const index = el.dataset.index;
-    tasks[index].done = !tasks[index].done;
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    populateList(tasks, taskList);
-}
-
-function removeDone(e) {
-    let index = e.target.parentNode.firstElementChild.dataset.index;
-    console.log(index);
-    tasks.splice(index, 1);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    e.target.parentNode.remove();
-}
-
-function changeTask(e) {
-    e.preventDefault();
-    let changeValueNumber = (document.querySelector('[name=change-to-do-text]').value)
-    populateList(tasks, taskList);
-}
-
-
-addTasks.addEventListener('submit', addTask);
-taskList.addEventListener('click', toggleDone);
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.btn-rem').forEach(item =>
-        item.addEventListener('click', removeDone))
-});
-changeTasks.addEventListener('submit', changeTask);
-populateList(tasks, taskList);

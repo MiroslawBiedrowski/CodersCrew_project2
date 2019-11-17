76279dfa90dfe751fe8622a50c53b7cd3747202a
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2019-11-17 12:24:26 +0100

    Add FN toggleDOne

--- main.js
+++ main.js
@@ -5,9 +5,27 @@ function addTask(task) {
         task,
         done: false
     });
-    // localStorage.setItem('tasks1', JSON.stringify(tasks));
-    console.log(`Masz do zrobienia poniższą poniższe rzeczy:`);
+    localStorage.setItem('tasks1', JSON.stringify(tasks));
+    console.log(`Aktualnie masz do zrobienia poniższą poniższe rzeczy:`);
     tasks.forEach((element, index) => {
         console.log(index, element)
     });
-}
+};
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    localStorage.setItem('tasks1', JSON.stringify(tasks));
+    console.log(`Aktualnie masz do zrobienia poniższą poniższe rzeczy:`);
+    tasks.forEach((element, index) => {
+        console.log(index, element)
+    });
+};
+
+function toggleTask(index) {
+    tasks[index].done = !tasks[index].done;
+    localStorage.setItem('tasks1', JSON.stringify(tasks));
+    console.log(`Aktualnie masz do zrobienia poniższą poniższe rzeczy:`);
+    tasks.forEach((element, index) => {
+        console.log(index, element)
+    });
+};
